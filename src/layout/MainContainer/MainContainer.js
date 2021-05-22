94edc586e4866170b03402ce5ad125bc6c039ec6
Luis E. Jiménez Robles <luisejimenezrobles@gmail.com>
2021-05-22 15:57:44 -0500

    handleView implemented

--- src/layout/MainContainer/MainContainer.js
+++ src/layout/MainContainer/MainContainer.js
@@ -1,9 +1,19 @@
 import React from "react";
 
-const MainContainer = () => {
+const MainContainer = ({ view = 0 }) => {
+  const handleView = () => {
+    switch (view) {
+      case 0:
+        return <h1>Caso 0 entre</h1>;
+      case 1:
+        return <h1>Caso 1 entre</h1>;
+      default:
+        return <h1>UPS</h1>;
+    }
+  };
   return (
-    <div className="w-3/4 h-5/6 mx-4 shadow-lg p-2 rounded-lg bg-white">
-      <h1>HEEEy</h1>
+    <div className="w-4/5 h-5/6 mx-4 shadow-lg p-2 rounded-lg bg-white">
+      {handleView()}
     </div>
   );
 };

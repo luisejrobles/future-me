cde13c02c2334fae36d7d1102b8a45fee81c9f76
Luis E. Jiménez Robles <luisejimenezrobles@gmail.com>
2021-05-22 15:57:14 -0500

    options fixed

--- src/layout/LeftNav/LeftNav.js
+++ src/layout/LeftNav/LeftNav.js
@@ -1,13 +1,21 @@
 import React from "react";
 
-const LeftNav = () => {
+const LeftNav = ({ updateState }) => {
+  const changeView = (value) => {
+    console.log(value);
+  };
+
   return (
-    <div className="w-1/4 bg-black-300 m-2 font-sans text-2xl divide-y">
-      <ul className="text-white">
-        <li>Mi guardadito</li>
-        <li>Retiro</li>
-        <li>Calcular mi inversión</li>
-      </ul>
+    <div className="w-1/5 bg-black-300 m-2 font-sans text-2xl divide-y">
+      <button className="border-none border-radius-lg text-left w-4/5 text-white hover:bg-indigo-900">
+        Mi guardadito
+      </button>
+      <button className="border-none border-radius-lg text-left w-4/5 text-white hover:bg-indigo-900">
+        Mi Retiro
+      </button>
+      <button className="border-none border-radius-lg text-left w-4/5 text-white hover:bg-indigo-900">
+        Inversión
+      </button>
     </div>
   );
 };

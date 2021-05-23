61b73e9bc0532266fefb63c1698341b4afee2bc6
Luis E. Jiménez Robles <luisejimenezrobles@gmail.com>
2021-05-22 23:05:57 -0500

    Footer done

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -1,31 +1,48 @@
 import React from "react";
-import { Facebook, GitHub, Instagram, Twitter } from "react-feather";
+import {
+  Facebook,
+  GitHub,
+  Instagram,
+  Linkedin,
+  Twitter,
+  User,
+} from "react-feather";
 
 const Footer = () => {
   return (
     <div className=" flex flex-col text-center font-sans text-xl text-indigo-900">
       <div className="self-center">Made with ♥ by luisejrobles </div>
-      <div className="self-center flex">
+      <div className="self-center flex space-x-4">
         <a
           href="https://twitter.com/luisejrobles"
           rel="noreferrer"
           target="_blank"
         >
-          <Twitter color="#1A237E" />
+          <Twitter color="#322E81" />
         </a>
         <a
           href="https://github.com/luisejrobles"
           rel="noreferrer"
           target="_blank"
         >
-          <GitHub color="#1A237E" />
+          <GitHub color="#322E81" />
         </a>
         <a
           href="https://instagram.com/luisejrobles"
           rel="noreferrer"
           target="_blank"
         >
-          <Instagram color="#1A237E" />
+          <Instagram color="#322E81" />
+        </a>
+        <a href="https://lu.ma/luisejrobles" rel="noreferrer" target="_blank">
+          <User color="#322E81" />
+        </a>
+        <a
+          href="https://linkedin.com/in/luisejrobles"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <Linkedin color="#322E81" />
         </a>
       </div>
     </div>

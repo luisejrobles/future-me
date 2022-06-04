e505897a2deb87f3bd5ffa381c6b2cb13620a67b
Luis E. Jiménez Robles <luisejimenezrobles@gmail.com>
2022-06-04 00:46:53 -0500

    added alt text to dog gif at Inversion.js

--- src/components/Inversion/Inversion.js
+++ src/components/Inversion/Inversion.js
@@ -6,6 +6,7 @@ const Inversion = () => {
       TBA
       <img
         className="w-64 self-center "
+        alt="winnier dressed as cop being followed by dog dressed as ambulance"
         src="https://i.gifer.com/7uQm.gif"
       ></img>
     </div>

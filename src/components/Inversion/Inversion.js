e71683a8c3e4628fcd8d1866e3e03c90fb97b9e8
Luis E. Jiménez Robles <luisejimenezrobles@gmail.com>
2022-06-04 00:44:59 -0500

    winnier at Inversion. V important

--- src/components/Inversion/Inversion.js
+++ src/components/Inversion/Inversion.js
@@ -4,6 +4,10 @@ const Inversion = () => {
   return (
     <div className="text-9xl justify-center text-center text-indigo-300 h-full flex flex-col ">
       TBA
+      <img
+        className="w-64 self-center "
+        src="https://i.gifer.com/7uQm.gif"
+      ></img>
     </div>
   );
 };
